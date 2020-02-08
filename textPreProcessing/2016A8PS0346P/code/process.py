718e507f878fd4c86a4bfedf01c6a7f5eb1aa525
lolcoder <chetan.arora02@gmail.com>
2020-02-08 07:24:29 +0530

    Some changes

--- textPreProcessing/2016A8PS0346P/code/process.py
+++ textPreProcessing/2016A8PS0346P/code/process.py
@@ -14,28 +14,57 @@ from nltk.corpus import wordnet
 #Stemming PorterStemmer
 from nltk.stem import PorterStemmer
 
-#lemmatizing WordNetlemmatizer 
+#lemmatizing WordNetLemmatizer 
 from nltk.stem import WordNetLemmatizer
 
 
 #HTML parsing library
 import bs4
 
-def plot(tokenList):
-	p = nltk.FreqDist(tokenList)
-	pid = os.fork()
-	if pid==0:
-		p.plot(30,cumulative=False)
-		sys.exit()
+def plot(tokenList,title):
+	p.plot(30,cumulative=False,title=title)
 	return
 
-def findPatterns(listOfWords):
+def mostOccuring(dist,p):
+	myDict = dist.most_common(len(dist))
+	count = 0
+	val = 0
+	for i,j in myDict:
+		val += j
+		count += 1
+		if(val >= p):
+			break
+	return str(count)
+
+def findPatterns(listOfWords,title):
 	unigrams = listOfWords
-	bigrams = ngrams(listOfWords,2)
-	trigrams = ngrams(listOfWords,3)
-	plot(listOfWords)
-	plot(bigrams)
-	plot(trigrams)
+	bigrams = list(ngrams(listOfWords,2))
+	print("Total number of bigrams:"+str(len(bigrams)))
+	trigrams = list(ngrams(listOfWords,3))
+	print("Total number of trigrams:"+str(len(trigrams)))
+	pid = os.fork()
+	if pid==0:
+		#Unigrams
+		dist = nltk.FreqDist(listOfWords)
+		print("Number of "+title+" unigrams that make up 90%:"+mostOccuring(dist,0.9*len(listOfWords)))
+		print("Number of unique unigrams after"+title+str(len(dist.keys())))
+		print("Exit unigrams plot to get bigram plots")
+		dist.plot(30,cumulative=False,title=title+' Unigrams')
+
+		#Bigrams
+		print("Exit Bigrams plot to get trigram plots")
+		dist = nltk.FreqDist(bigrams)
+		print("Number of "+title+" bigrams that make up 80%:"+mostOccuring(dist,0.8*len(listOfWords)))
+		print("Number of unique bigrams after"+title+str(len(dist.keys())))
+		dist.plot(30,cumulative=False,title=title+' Bigrams')		
+
+		#Trigrams
+		dist = nltk.FreqDist(trigrams)
+		print("Number of "+title+" trigrams that make up 70%:"+mostOccuring(dist,0.7*len(listOfWords)))
+		print("Number of unique trigrams after"+title+str(len(dist.keys())))
+		dist.plot(30,cumulative=False,title=title+' Trigrams')				
+		sys.exit()
+	return
 def tagWordNet(treebank_tag):
     if treebank_tag.startswith('J'):
         return wordnet.ADJ
@@ -48,7 +77,11 @@ def tagWordNet(treebank_tag):
     else:
         return wordnet.NOUN
 # Taking input from file
-f = open('../wiki_16','r')
+if len(sys.argv)!=2:
+	print("Usage:")
+	print("python3 process.py file")
+	sys.exit()
+f = open(sys.argv[1],'r')
 unparsedData = f.read()
 f.close()
 
@@ -65,15 +98,16 @@ data = ' '.join([tag.getText().lower() for tag in docList])
 # 		data = data.replace(w,' ' if w in ('\n','  ') else '')
 
 #Using nltk libraries for making ngrams
-listOfWords = [x for x in regexp_tokenize(data,r'[?\s(),.&-"]', gaps=True) if x not in ('',' ')]
+listOfWords = [x for x in regexp_tokenize(data,r'[?"\s(),.&\-]', gaps=True) if x not in ('',' ')]
 
-# #Without stemming and lemmatization
-# findPatterns(listOfWords)
+print("Total number of unique Tokenized words:")
+#Without stemming and lemmatization
+findPatterns(listOfWords,'Tokenized')
 
-# # Stemming process
-# stemmer = PorterStemmer()
-# stemProcessedWords = [stemmer.stem(w) for w in listOfWords]
-# findPatterns(stemProcessedWords)
+# Stemming process
+stemmer = PorterStemmer()
+stemProcessedWords = [stemmer.stem(w) for w in listOfWords]
+findPatterns(stemProcessedWords,'Stemmed')
 
 # lemmatization process
 # Pos tagging
@@ -82,38 +116,39 @@ wordNetTaggedWords = [tagWordNet(x[1]) for x in taggedWords]
 
 lemmatizer = WordNetLemmatizer()
 lemmatizedWords = [lemmatizer.lemmatize(listOfWords[x],wordNetTaggedWords[x]) for x in range(len(taggedWords))]
-findPatterns(lemmatizedWords)
+findPatterns(lemmatizedWords,'Lemmatized')
 
-# #Chi-Square test for finding bi-gram collocations
-# bigrams = [x for x in ngrams(listOfWords,2)]
-# # Occurances and Expentances
-# ocs = {}
-# firstOcs = {}
-# lastOcs = {}
-# chiValues = {}
-# for bigram in bigrams:
-# 	if bigram in ocs:
-# 		ocs[bigram] += 1
-# 	else:
-# 		ocs[bigram] = 1
-# 		if bigram[0] in firstOcs:
-# 			firstOcs[bigram[0]] += 1
-# 		else:
-# 			firstOcs[bigram[0]] = 1
-# 		if bigram[1] in lastOcs:
-# 			lastOcs[bigram[1]] += 1
-# 		else:
-# 			lastOcs[bigram[1]] = 1
+#Chi-Square test for finding bi-gram collocations
+bigrams = [x for x in ngrams(listOfWords,2)]
+# Occurances and Expentances
+ocs = {}
+firstOcs = {}
+lastOcs = {}
+chiValues = {}
+for bigram in bigrams:
+	if bigram in ocs:
+		ocs[bigram] += 1
+	else:
+		ocs[bigram] = 1
+		if bigram[0] in firstOcs:
+			firstOcs[bigram[0]] += 1
+		else:
+			firstOcs[bigram[0]] = 1
+		if bigram[1] in lastOcs:
+			lastOcs[bigram[1]] +=1	
+		else:
+			lastOcs[bigram[1]] = 1
 
-# N = len(bigrams)
-# for bg in ocs:
-# 	O = ocs[bg]
-# 	E = (firstOcs[bg[0]]*lastOcs[bg[1]])/N
-# 	chiSquare = (O-E)*(O-E)/E
-# 	#Checking if chiSquare is comparable to 3.8 (probability distribution for 0.05)
-# 	chiValues[bg] = chiSquare
+N = len(bigrams)
+for bg in ocs:
+	O = ocs[bg]
+	E = (firstOcs[bg[0]]*lastOcs[bg[1]])/N
+	chiSquare = (O-E)*(O-E)/E
+	#Checking if chiSquare is comparable to 3.8 (probability distribution for 0.05)
+	chiValues[bg] = chiSquare
 
-# sorted_values = sorted(chiValues.items(),key=operator.itemgetter(1),reverse=True)
+sorted_values = sorted(chiValues.items(),key=operator.itemgetter(1),reverse=True)
 
-# for i in range(20):
-# 	print(sorted_values[i][0],end=',')
+print("Collocations found in the text:")
+for i in range(20):
+	print(sorted_values[i][0])
